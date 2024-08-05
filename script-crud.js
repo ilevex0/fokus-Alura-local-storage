2f38483b40fa0a2d79efbccc0baca44fd20acd1b
Pedro Otávio <pedrootavio09@yahoo.com.br>
2024-08-04 22:29:24 -0300

    Final Project

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,7 @@ const ulTarefas = document.querySelector(".app__section-task-list");
 const paragrafoDescricaoTarefa = document.querySelector(".app__section-active-task-description");
 
 const btnRemoverConcluidas = document.querySelector("#btn-remover-concluidas");
+const btnRemoverTodas = document.querySelector("#btn-remover-todas");
 
 let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 let tarefaSelecionada = null;
@@ -108,11 +109,14 @@ document.addEventListener("FocoFinalizado", () => {
     }
 })
 
-btnRemoverConcluidas.onclick = () => {
-    const seletor = ".app__section-task-list-item-complete";
+const removerTarefas = (somenteCompletas) => {
+    const seletor = somenteCompletas ? ".app__section-task-list-item-complete" : ".app__section-task-list-item";
+
     document.querySelectorAll(seletor).forEach(elemento => {
         elemento.remove();
     })
-    tarefas = tarefas.filter(elemento => !elemento.completa);
+    tarefas = somenteCompletas ? tarefas.filter(elemento => !elemento.completa) : [];
     atualizarTarefas();
 }
+btnRemoverConcluidas.onclick = () => removerTarefas(true);
+btnRemoverTodas.onclick = () => removerTarefas(false);
